ea00cec1636829009c853983c7695186fdb0a172
Radley Cardente <radleycardente@gmail.com>
2020-09-24 22:28:33 +1000

    Adding log in via spotify

--- party/src/app/app.module.ts
+++ party/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { LoginCardComponent } from './login/login-card/login-card.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginComponent } from "./login/login.component";
+import { LoginCardComponent } from "./login/login-card/login-card.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { SpotifyService } from "./service/spotify.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, LoginCardComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule, MatButtonModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+  ],
+  providers: [SpotifyService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- party/src/app/login/login-card/login-card.component.ts
+++ party/src/app/login/login-card/login-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login-card',
   templateUrl: './login-card.component.html',
-  styleUrls: ['./login-card.component.scss']
+  styleUrls: ['./login-card.component.scss'],
 })
 export class LoginCardComponent implements OnInit {
+  @Output() logInPressed = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onLoginPressed() {
+    window.location.href = 'https://accounts.spotify.com/authorize?' +
+    'client_id=091f4e7a51b145c9843854c6aa29c317&' +
+    'response_type=token&' +
+    'redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Froom&' +
+    'scope=app-remote-control';
+ }
 }
